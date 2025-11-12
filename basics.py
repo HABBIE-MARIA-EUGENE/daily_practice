735c4706c28c1917be896096ae8a40c79abdb941
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2025-11-12 12:45:19 +0530

    added

---
+++ basics.py
@@ -0,0 +1,43 @@
+name = "habbie maria eugene m"
+
+
+print(name.upper())
+
+name=name.upper()
+print(name)
+
+name=name.lower()
+print(name)
+
+print(name.find('e'))
+print(name.find('eugene'))
+
+string = "python for all"
+
+print(string)
+
+print(string.replace('for','4'))
+print(string)
+
+print("python" in string)
+
+print(1+1)
+print(1-1)
+print(3/2)
+print(3//2)
+print(3*2)
+print(3**2)
+
+
+x = 10
+
+#x = x + 3
+
+x += 3
+print(x)
+
+x = 10 + 3 * 2
+print(x)
+
+x = (10 + 3) * 2
+print(x)
